6d55cf0db688c4ad4e79c0cd91029458187cdd1b
Weili Yi <wlyi@thoughtworks.com>
2020-05-07 17:58:47 +0800

    refact: split move rental amount from Customer

--- src/main/java/cn/xpbootcamp/refactor/Customer.java
+++ src/main/java/cn/xpbootcamp/refactor/Customer.java
@@ -29,22 +29,7 @@ public class Customer {
             Rental each = rentals.nextElement();
             //show figures for this rental
             //determine amounts for each line
-            double thisAmount = 0d;
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.HISTORY:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CAMPUS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            double thisAmount = each.getAmount();
             //add frequent renter points
             frequentRenterPoints++;
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
--- src/main/java/cn/xpbootcamp/refactor/Rental.java
+++ src/main/java/cn/xpbootcamp/refactor/Rental.java
@@ -18,4 +18,25 @@ public class Rental {
         return daysRented;
     }
 
+    double getAmount() {
+        double thisAmount = 0d;
+        int daysRented = this.getDaysRented();
+        switch (this.movie.getPriceCode()) {
+            case Movie.HISTORY:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CAMPUS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+
 }
